bea9a031090387057b57d3fdf97c41ccb099ee19
CaptSiro <honzahorak433@icloud.com>
2024-08-27 10:15:00 +0200

    Migrated Response from RoutePass

---
+++ src/core/Response.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace core;
+
+class Response {
+    // Informational
+    public const CODE_CONTINUE = 100;
+    public const CODE_SWITCHING_PROTOCOLS = 101;
+
+    // Successful
+    public const CODE_OK = 200;
+    public const CODE_CREATED = 201;
+    public const CODE_ACCEPTED = 202;
+    public const CODE_NON_AUTHORITATIVE_INFORMATION = 203;
+    public const CODE_NO_CONTENT = 204;
+    public const CODE_RESET_CONTENT = 205;
+    public const CODE_PARTIAL_CONTENT = 206;
+
+    // Redirection
+    public const CODE_MULTIPLE_CHOICES = 300;
+    public const CODE_MOVED_PERMANENTLY = 301;
+    public const CODE_FOUND = 302;
+    public const CODE_SEE_OTHER = 303;
+    public const CODE_NOT_MODIFIED = 304;
+    public const CODE_USE_PROXY = 305;
+
+    // Client Error
+    public const CODE_BAD_REQUEST = 400;
+    public const CODE_UNAUTHORIZED = 401;
+    public const CODE_PAYMENT_REQUIRED = 402;
+    public const CODE_FORBIDDEN = 403;
+    public const CODE_NOT_FOUND = 404;
+    public const CODE_METHOD_NOT_ALLOWED = 405;
+    public const CODE_NOT_ACCEPTABLE = 406;
+    public const CODE_PROXY_AUTHENTICATION_REQUIRED = 407;
+    public const CODE_REQUEST_TIMEOUT = 408;
+    public const CODE_CONFLICT = 409;
+    public const CODE_GONE = 410;
+    public const CODE_LENGTH_REQUIRED = 411;
+    public const CODE_PRECONDITION_FAILED = 412;
+    public const CODE_PAYLOAD_TOO_LARGE = 413;
+    public const CODE_URI_TOO_LONG = 414;
+    public const CODE_UNSUPPORTED_MEDIA_TYPE = 415;
+
+    // Server Error
+    public const CODE_INTERNAL_SERVER_ERROR = 500;
+    public const CODE_NOT_IMPLEMENTED = 501;
+    public const CODE_BAD_GATEWAY = 502;
+    public const CODE_SERVICE_UNAVAILABLE = 503;
+    public const CODE_GATEWAY_TIMEOUT = 504;
+    public const CODE_HTTP_VERSION_NOT_SUPPORTED = 505;
+
+
+
+    private array $headers;
+
+
+
+    public function __construct() {
+        $this->headers = [];
+    }
+
+
+
+    public function hasHeader(string $header): bool {
+        return isset($this->headers[$header]);
+    }
+
+    public function setHeader(string $header, string $value): void {
+        $this->headers[$header] = $value;
+    }
+
+    /**
+     * @param array ...$headers Single header is tuple of two strings, name and value. Example: <code>"Location: /"</code> would be
+     * <code>["Location", "/"]</code>
+     * @return void
+     */
+    public function setAllHeaders(array ...$headers): void {
+        foreach ($headers as $header) {
+            $this->headers[$header[0]] = $header[1];
+        }
+    }
+
+    public function removeHeader(string $header): void {
+        unset($this->headers[$header]);
+    }
+
+    public function removeAllHeaders(): void {
+        $this->headers = [];
+    }
+
+    public function setStatus(int $code): void {
+        http_response_code($code);
+    }
+
+    public function generateHeaders(): void {
+        foreach ($this->headers as $header => $value) {
+            header("$header: $value");
+        }
+    }
+
+    /**
+     * Exits the execution without sending any data but headers will be sent.
+     */
+    public function flush(): void {
+        $this->generateHeaders();
+        exit();
+    }
+
+    /**
+     * Alias for flush function
+     * @see Response::flush()
+     */
+    public function end(): void {
+        $this->flush();
+    }
+
+    /**
+     * Exits the execution.
+     *
+     * Sends string data to user.
+     */
+    public function send($text): void {
+        echo $text;
+        $this->flush();
+    }
+
+    /**
+     * Exits the execution.
+     *
+     * Parses object into JSON text representation and sends it to the user.
+     */
+    public function json($data, $flags = 0, $depth = 512): void {
+        echo json_encode($data, $flags, $depth);
+        $this->flush();
+    }
+
+    /**
+     * Exits the execution with error code and message.
+     */
+    public function error(string $message, int $httpStatusCode = -1): void {
+        // todo different outputs per response type
+
+        if ($httpStatusCode !== -1) {
+            $this->setStatus($httpStatusCode);
+        }
+
+        $this->send($message);
+    }
+
+    /**
+     * Exits the execution.
+     *
+     * Reads file and sends it contents to the user.
+     *
+     * **This function does not download the file on user's end. It only sends file's contents.**
+     */
+    public function readFile(string $file): void {
+        if (!file_exists($file)) {
+            $this->error("RequestFile not found: $file", self::CODE_NOT_FOUND);
+        }
+
+        readfile($file);
+        $this->flush();
+    }
+
+    /**
+     * Exits the execution.
+     *
+     * Checks for valid file path and sets headers to download it.
+     */
+    public function download(string $file): void {
+        $this->setAllHeaders(
+            [Http::HEADER_CONTENT_DESCRIPTION, "RequestFile Transfer"],
+            [Http::HEADER_CONTENT_TYPE, 'application/octet-stream'],
+            [Http::HEADER_CONTENT_DISPOSITION, "attachment; filename=" . basename($file)],
+            [Http::HEADER_PREGMA, "public"],
+            [Http::HEADER_CONTENT_LENGTH, filesize($file)]
+        );
+
+        $this->readFile($file);
+    }
+
+    public function render(Render $render, ?string $template = null, bool $doFlushResponse = true): void {
+        echo $render->render($template);
+        if (!$doFlushResponse) {
+            return;
+        }
+
+        $this->flush();
+    }
+
+    /**
+     * Redirects request to new URL.
+     *
+     * Do prepend home directory is used to dynamically prepend directory structure that is between server directory and this project's directory.
+     *
+     * www/ **my-project** /index.php -> localhost/ **my-project** / **'/my-project'** will be prepended
+     *
+     * `/api/user` -> `/my-project/api/user`
+     *
+     *
+     * @param string $url accepts same URLs as Location header.
+     * @param bool $doPrependHomeDirectory
+     * @return void
+     */
+    public function redirect(string $url, bool $doPrependHomeDirectory = true): void {
+        $this->setHeader("Location", ($doPrependHomeDirectory ? $_SERVER["HOME_DIR"] : "") . $url); // todo App::getHomeDir()
+        $this->flush();
+    }
+}
