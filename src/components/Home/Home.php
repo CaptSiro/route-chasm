65d00fc9dd193a76d34526774cc525fcbd4ffc0f
CaptSiro <honzahorak433@icloud.com>
2025-08-30 16:43:23 +0200

    Clean up

--- src/components/Home/Home.php
+++ src/components/Home/Home.php
@@ -3,13 +3,8 @@
 namespace components\Home;
 
 use components\core\HtmlHead\HtmlHead;
-use components\core\Icon;
 use components\core\Menu\Menu;
-use components\core\Message\Message;
-use components\core\RoutedMenu\RoutedMenu;
 use components\core\WebPage\ContextAwareWebPage;
-use core\route\Route;
-use core\route\RouteNode;
 use core\view\ContainerContent;
 
 class Home extends ContainerContent {
@@ -21,35 +16,5 @@ class Home extends ContainerContent {
                 head: new HtmlHead("Home")
             )
         );
-    }
-
-
-
-    public function onBind(RouteNode $bindingPoint): void {
-        parent::onBind($bindingPoint);
-
-        $router = $bindingPoint
-            ->getRouter()
-            ->get('/home');
-
-        $router->use(
-            Route::menu("/Foo")
-                ->icon("Foo", Icon::nf('nf-md-web')),
-            new Message("Foo")
-        );
-
-        $router->use(
-            Route::menu("/Foo/Bar")
-                ->icon("Bar", Icon::nf('nf-md-laptop_account')),
-            new Message("Bar")
-        );
-
-        $router->use(
-            Route::menu("/Fizz Buzz")
-                ->icon("Fizz Buzz", Icon::nf('nf-cod-settings_gear')),
-            new Message("Fizz Buzz")
-        );
-
-        $this->menu = RoutedMenu::from($router);
     }
 }
